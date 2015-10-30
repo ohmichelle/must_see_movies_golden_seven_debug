0f8f463d75643acde17d4ac7e7ddd0ec94895148
ohmichelle <oh.michelles@gmail.com>
2015-10-29 23:51:11 -0500

    movies/directors debug

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,14 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
-    @director = Director.find(params[:id])
+    #Parameters: {"id"=>"1"}
+    @director = Director.find(params["id"])
   end
 
-  def new
+  def new_form
   end
 
   def create_row
@@ -19,7 +20,7 @@ class DirectorsController < ApplicationController
 
     @director.save
 
-    render("show")
+    render("show.html.erb")
   end
 
   def edit_form
@@ -29,14 +30,14 @@ class DirectorsController < ApplicationController
   def update_row
     @director = Director.find(params[:id])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
-    render("show")
+    render("show.html.erb")
   end
 
   def destroy
